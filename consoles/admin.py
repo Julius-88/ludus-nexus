28d650f3552f3d8cd69e6422b728caeda005576e
Julius Alamarvdashti <drako886@gmail.com>
2023-11-29 22:36:58 +0000

    Add database schema for consoles app

--- consoles/admin.py
+++ consoles/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Console, Product, Tag, ProductTag, Wishlist
 
-# Register your models here.
+
+admin.site.register(Console)
+admin.site.register(Product)
+admin.site.register(Tag)
+admin.site.register(ProductTag)
+admin.site.register(Wishlist)
