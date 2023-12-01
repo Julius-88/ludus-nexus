4a72e0ae6515066459a2c503dd4bf83a149514d4
Julius Alamarvdashti <drako886@gmail.com>
2023-12-01 12:35:06 +0000

    Update NewsArticle Model in home.html

--- home/models.py
+++ home/models.py
@@ -3,14 +3,18 @@ from django.conf import settings
 
 
 class NewsArticle(models.Model):
-    title = models.CharField(max_length=100, null=False, blank=False)
+    title = models.CharField(
+        max_length=200,
+        null=False,
+        blank=False,
+        unique=True)
     content = models.TextField()
     summary = models.CharField(max_length=255, null=False, blank=False)
-    image_url = models.URLField(max_length=1024, blank=False)
+    image_url = models.ImageField(blank=False, null=False)
     source_url = models.URLField(max_length=1024, blank=False)
 
     def __str__(self):
-        return f"{self.title} - {self.name}"
+        return self.title
 
 
 class Event(models.Model):
