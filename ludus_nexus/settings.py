1b7687e3dfa56556cb3c43d2c3c26b8e1349b5a2
Julius Alamarvdashti <drako886@gmail.com>
2023-12-06 09:51:04 +0000

    Update settings.py with the removed key

--- ludus_nexus/settings.py
+++ ludus_nexus/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-9#eq-cnlynfrb)a=fvv@)2wu^m69qlz57wvz$a-)-ep)u*jy_y')
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 
