6a05b3c4857c90d80551144eb51fa9b5343edda0
Julius Alamarvdashti <drako886@gmail.com>
2024-02-11 09:24:43 +0000

    Add styling to Add product page

--- consoles/forms.py
+++ consoles/forms.py
@@ -6,3 +6,7 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['console', 'tags', 'name', 'description', 'price', 'image']
+        help_texts = {
+            'tags': 'In order to select more than one tag, hold down "Ctrl" '
+            'or "Cmd" on a Mac, and left click on each tag you want.'
+        }
